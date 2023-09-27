58838f5c1059cfca6c34c19399d296786df7009a
avi <avi@verma.com>
2023-09-27 20:26:59 +0530

    let var and const

--- 1.js
+++ 1.js
@@ -1,1 +1,16 @@
-console.log("Avinash")
+const accountId=14455
+let accountEmail= "1234@gmail.com"
+var accountPassword="1234"
+accountCity="Dehli"
+
+// accountId=2 // Not Allowed because its const
+accountEmail="123@gmail.com"
+accountPassword="Banglore"
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword])
+
+/*
+Prefer not to use var because of issue in block scope and function scope
+*/
